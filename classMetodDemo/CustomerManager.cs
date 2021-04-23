41ad406cf4e9174eac09822e2b26e6571827c323
oguzhan35 <oguzhandemir35@gmail.com>
2021-04-24 00:12:35 +0300

    demo

---
+++ classMetodDemo/CustomerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classMetodDemo
+{
+   public class CustomerManager
+    {
+      public  void Add(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName+"   Eklendi");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + "   Silindi");
+        }
+
+        public void List(Customer customer)
+        {
+                Console.WriteLine(customer.FirstName +"\n"+customer.LastName+"  listelendi" );        
+        }
+
+        
+    }
+}
---
+++ classMetodDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace classMetodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "Ali";
+            customer1.LastName = "atsak";
+            customer1.Address = "Aydın";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.FirstName = "Pınar";
+            customer2.LastName = "bicer";
+            customer2.Address = "Aydın";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Senay";
+            customer3.LastName = "aygun";
+            customer3.Address = "Aydın";
+
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer1);
+            customerManager.Delete(customer2);
+            customerManager.List(customer3);
+             
+        }
+    }
+}
